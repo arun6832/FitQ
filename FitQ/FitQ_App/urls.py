9e943146dc7ca9ef60eb106f1b2bcf7cae267d65
Arun <arunpappulli@gmail.com>
2024-09-04 07:57:13 +0530

    new

--- FitQ/FitQ_App/urls.py
+++ FitQ/FitQ_App/urls.py
@@ -1,7 +1,11 @@
+from django.contrib import admin
 from django.urls import path
-from . import views
+from FitQ_App import views  # Import your views here
 
-urlpatterns=[
-    path('',views.index, name="index"),
-    path('sign_in',views.sign_in,name="sign_in"),
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('sign_in/', views.sign_in, name='sign_in'),
+    path('auth_receiver/', views.auth_receiver, name='auth_receiver'),
+    path('sign-out/', views.sign_out, name='sign_out'),
 ]
